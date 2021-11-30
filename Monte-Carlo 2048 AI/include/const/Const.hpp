119abfed7fcb1d8d3ec5442c19c7729d8191f036
Chupakabra0 <rootalfa4@gmail.com>
2021-12-01 01:43:46 +0200

    Fix: change const's types

--- Monte-Carlo 2048 AI/include/const/Const.hpp
+++ Monte-Carlo 2048 AI/include/const/Const.hpp
@@ -1,5 +1,5 @@
 #pragma once
 
-static constexpr auto WINDOW_MARGIN  = 100.0f;
-static constexpr auto WINDOW_WIDTH   = 400.0f;
+static constexpr auto WINDOW_MARGIN  = 100u;
+static constexpr auto WINDOW_WIDTH   = 400u;
 static constexpr auto WINDOW_HEIGHT  = WINDOW_WIDTH + WINDOW_MARGIN;
